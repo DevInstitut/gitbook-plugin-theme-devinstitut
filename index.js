a43918cc372ae79d951931e78e6f456b2eb8eb8a
Rossi Oddet <rossi.oddet@dev-institut.fr>
2018-05-15 12:17:23 +0200

    feat: insert logo dev institut

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
+//const insertLogoPlugin = require('./_assets/plugins/gitbook-plugin-insert-logo');
+
+const bookConfig = {js: [], css: []};
+
+
+const addPlugin = (cfg, plugin) => {
+    const plug = require(plugin);
+    if (plug.book) {
+        [ 'js', 'css']
+            .forEach(
+                prop => cfg[prop] = cfg[prop].concat(plug.book[prop].map(v => `${plugin}/${plug.book['assets']}/${v}`)))
+    }
+};
+
+addPlugin(bookConfig, './_assets/plugins/gitbook-plugin-insert-logo');
+
+console.log("bookConfig", bookConfig)
 
 module.exports = {
+    book: bookConfig,
     hooks: {
-        config: function(config) {
+        config: function (config) {
+
             config.styles = config.styles || config.pluginsConfig['theme-default'].styles;
+
+            config.pluginsConfig['insert-logo'] = {
+                "url": "http://www.dev-institut.fr/img/logo.svg",
+                "style": "width: 50px"
+            };
 
             return config;
         }
